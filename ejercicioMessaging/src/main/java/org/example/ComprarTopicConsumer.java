2cd7d2a4b594e4d47ed3d8c263ffd133eeca367e
agostinaluciaietri <130680622+agostinaluciaietri@users.noreply.github.com>
2023-11-07 17:50:57 -0300

    Creación de topic para Banco y Usuario
    Debugging

--- ejercicioMessaging/src/main/java/org/example/ComprarTopicConsumer.java
+++ ejercicioMessaging/src/main/java/org/example/ComprarTopicConsumer.java
@@ -3,24 +3,23 @@ package org.example;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
-public class ComprarTopicPublisher {
+public class ComprarTopicConsumer {
     public static void main(String[] args) throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-        String valor = "404.0";
-        String cotizacion = "10.5";
-
-        String message = "Deseo comprar " + valor + " Javacoin a una cotización de " + cotizacion;
-
-        channel.basicPublish("Usuario-Topic","comprar", null, message.getBytes());
-        System.out.println("Mensaje enviado: " + message);
-        channel.close();
-        connection.close();
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String (delivery.getBody());
+            System.out.println("Mensaje recibido: " + message);
+        };
+        channel.basicConsume("Comprar-queue", true, deliverCallback, consumerTag -> {});
     }
+
 }
+
